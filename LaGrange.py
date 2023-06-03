cb7104d951a6df8edbdb3a5bd759b5d6e88e12ea
Piotr Noga <Piotr02N@gmail.com>
2023-06-04 00:54:09 +0200

    deleted not used code

--- LaGrange.py
+++ LaGrange.py
@@ -1,16 +1,3 @@
-import numpy as np
-
-
-# def lagrange(data, x, index):
-#     wynik = 1
-#     i = 0
-#     while i < 512:
-#         wynik *= iloczyn(x, index, i)
-#         i += 64
-#     # wynik - fi_index(x)
-#     return wynik*data[index]
-
-
 def lagrange(x, xData, yData):
     n = len(xData)
     result = 0.0
@@ -23,12 +10,3 @@ def lagrange(x, xData, yData):
         result += term
 
     return result
-
-# y - rozpatrywany y, index - index x, number - numer iteracji
-# def iloczyn(x, index, number):
-#     if index == number:
-#         return 1
-#     else:
-#         licznik = x - number
-#         mianownik = index - number
-#         return licznik/mianownik
