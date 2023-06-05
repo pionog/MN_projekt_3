efc92e2da9ccabb910d8e826c33ee402c897daf1
Piotr Noga <Piotr02N@gmail.com>
2023-06-05 03:57:44 +0200

    added interpolation using splines

---
+++ Spline.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+def spline(xData, yData, distance, indicies_intervals):  # distance - indicies_intervals - steps between two indicies
+    how_many = round(xData.shape[0] / indicies_intervals) + 1  # how_many - number of interpolated points
+    n = 4 * (how_many - 1)  # n - number of equations
+    matrix = np.zeros((n, n))
+    result_matrix = np.zeros((n, 1))
+
+    x_interpolated = np.array(xData[::indicies_intervals])
+    y_interpolated = np.array(yData[::indicies_intervals])
+
+    # S0(x0) = f(x0) = [a0 * 1] + [b0 * 0] + [c0 * 0] + [d0 * 0] + [a1 * 0] + ...  -->  [a0 * 1] = f(x0)
+    matrix[0][0] = 1
+    result_matrix[0][0] = y_interpolated[0]
+
+    # S0(x1) = f(x1)  -->  [a0 * 1] + [b0 * h] + [c0 * h^2] + [d0 * h^3] = f(x1)
+    h = x_interpolated[1] - x_interpolated[0]
+    matrix[1][0] = 1
+    matrix[1][1] = h
+    matrix[1][2] = pow(h, 2)
+    matrix[1][3] = pow(h, 3)
+    result_matrix[1][0] = y_interpolated[1]
+
+    # S''0(x0) = 0  -->  [c0 * 1] = 0
+    matrix[2][2] = 1
+    result_matrix[2][0] = 0
+
+    # S''n-1(xn) = 0  -->  [cn-1 * 2] + [dn-1 * 6 * h] = 0
+    h = x_interpolated[how_many - 1] - x_interpolated[how_many - 2]
+    matrix[3][4 * (how_many - 2) + 2] = 2
+    matrix[3][4 * (how_many - 2) + 3] = 6 * h
+    result_matrix[3] = 0
+
+    for i in range(1, how_many-1):
+        h = x_interpolated[i] - x_interpolated[i - 1]
+        # Si(xi) = f(xi)  -->  [ai * 1] = f(xi)
+        matrix[4 * i][4 * i] = 1
+        result_matrix[4 * i][0] = y_interpolated[i]
+
+        # Si(xi+1) = f(x+1)  -->  [ai * 1] + [bi * h] + [ci * h^2] + [di * h^3] = f(xi+1)
+        matrix[4 * i + 1][4 * i] = 1
+        matrix[4 * i + 1][4 * i + 1] = h
+        matrix[4 * i + 1][4 * i + 2] = pow(h, 2)
+        matrix[4 * i + 1][4 * i + 3] = pow(h, 3)
+        result_matrix[4 * i + 1][0] = y_interpolated[i + 1]
+
+        # S'i-1(xi) = S'i(xi)  -->  [bi-1 * 1] + [ci-1 * 2 * h] + [di-1 * 3 * h^2] = [bi]  -->  [bi-1 * 1] + [ci-1 * 2 * h] + [di-1 * 3 * h^2] + [bi * (-1)] = 0
+        matrix[4 * i + 2][4 * (i - 1) + 1] = 1
+        matrix[4 * i + 2][4 * (i - 1) + 2] = 2 * h
+        matrix[4 * i + 2][4 * (i - 1) + 3] = 3 * pow(h, 2)
+        matrix[4 * i + 2][4 * i + 1] = -1
+        result_matrix[4 * i + 2][0] = 0
+
+        # S''i-1(xi) = S''i(xi)  -->  [ci-1 * 2] + [di-1 * 6 * h] = [ci * 2]  -->  [ci-1 * 2] + [di-1 * 6 * h] + [ci * (-2)] = 0
+        matrix[4 * i + 3][4 * (i - 1) + 2] = 2
+        matrix[4 * i + 3][4 * (i - 1) + 3] = 6 * h
+        matrix[4 * i + 3][4 * i + 2] = -2
+        result_matrix[4 * i + 3][0] = 0
+
+    inverted_matrix = np.linalg.inv(matrix)
+
+    wanted_matrix = np.matmul(inverted_matrix, result_matrix)
+
+
+    for i in range(how_many-1):
+        elevation = 0.0
+        if distance >= x_interpolated[i] and distance <= x_interpolated[i+1]:
+            for j in range(4):
+                h = distance - x_interpolated[i]
+                elevation += wanted_matrix[4 * i + j] * pow(h, j)
+            break
+
+    return elevation
--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import LaGrange
+import Spline
+
+SAMPLES = 480
 
 path = "dane/MountEverest.csv"
 f = open(path, "r")  # uchwyt do pliku
 data = list(csv.reader(f, delimiter=","))
 f.close()
 dataArray = np.array(data)
-xData = np.array(dataArray[:, 0]).astype(np.float64)
-yData = np.array(dataArray[:, 1]).astype(np.float64)
+xData = np.array(dataArray[:SAMPLES+1, 0]).astype(np.float64)
+yData = np.array(dataArray[:SAMPLES+1, 1]).astype(np.float64)
 
 fig, ax = plt.subplots()
 ax.plot(xData, yData)
@@ -22,18 +25,41 @@ plt.show()
 yLaGrange = []
 xLaGrange = []
 
-wysokosc = np.array(yData[::64])
-dlugosc = np.array(xData[::64])
-i = 0
-while i < 512:
-    yLaGrange.append(LaGrange.lagrange(xData[i], dlugosc, wysokosc))
-    xLaGrange.append(xData[i])
-    i += 64
+ile = 32
 
+wysokosc = np.array(yData[::ile])
+dlugosc = np.array(xData[::ile])
+
+odleglosc = xData[SAMPLES-1]
+i = 0
+while i <= odleglosc:
+    yLaGrange.append(LaGrange.lagrange(i, dlugosc, wysokosc))
+    xLaGrange.append(i)
+    i += 8
 
 fig, ax = plt.subplots()
+ax.plot(xData, yData)
 ax.plot(xLaGrange, yLaGrange)
-ax.set_title("Aproksymacja profilu wysokościowego")
+ax.set_title("Interpolacja LaGrange'a")
 ax.set_xlabel("Odległość")
 ax.set_ylabel("Wysokość")
 plt.show()
+
+sklejanePunkty = []
+krok = 8
+indeks = 0
+ileKrokow = round(odleglosc/krok)
+for i in range(ileKrokow):
+    sklejanePunkty.append(Spline.spline(xData, yData, i * krok, 32))
+
+x = np.arange(0, ileKrokow * krok, krok)
+y = np.array(sklejanePunkty).astype(np.float64)
+fig, ax = plt.subplots()
+ax.plot(xData, yData)
+ax.plot(x, y)
+ax.set_title("Interpolacja funkcjami sklejanymi")
+ax.set_xlabel("Dystans")
+ax.set_ylabel("Wysokość")
+plt.show()
+
+
