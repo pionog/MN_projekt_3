cb33870559845a36086491babf2d9aa40ed2f023
Piotr Noga <Piotr02N@gmail.com>
2023-06-06 09:52:57 +0200

    added measuring time

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 import LaGrange
 import Spline
 
 SAMPLES = 480  # limitation of samples taken from the file (default they have 512 samples)
 DENSITIES = [16, 32, 48, 80]  # list of possible densities
-DIVISION_DENSITY = DENSITIES[1]  # this variable means that for every x where 32*x there is an interpolated point
+DIVISION_DENSITY = DENSITIES[0]  # this variable means that for every x where 32*x there is an interpolated point
 HOW_MANY_POINTS = round(SAMPLES / DIVISION_DENSITY) + 1  # counter of interpolated points
 STEP = 8  # how much space is needed between each point when calculating interpolated values
-FILE_NAME = "SpacerniakGdansk"
+FILE_NAME = "GlebiaChallengera"
 
 ################################
 # reading file and setting variables to further work
@@ -40,6 +41,7 @@ plt.show()
 
 ################################
 # Lagrange interpolation
+start = time.time()  # measuring time for computing interpolation
 
 yLagrange = []  # making an empty list for y values
 xLagrange = []  # making an empty list for x values
@@ -52,6 +54,9 @@ while i <= TOTAL_DISTANCE:
     xLagrange.append(i)  # appending current step
     i += STEP  # increasing i by a step
 
+end = time.time()
+diff = end - start
+print("Czas trwania interpolacji Lagrange'a: {:10.4f} sekund".format(diff))
 fig, ax = plt.subplots()
 ax.plot(xData, yData, color='b', label="Oryginalne dane")
 ax.plot(xLagrange, yLagrange, color='orange', label="Wartości interpolowane")
@@ -67,10 +72,16 @@ plt.show()
 ################################
 # Spline interpolation
 
+start = time.time()
+
 spline_points = []  # making an empty list
 how_many_steps = round(TOTAL_DISTANCE / STEP)
 for i in range(how_many_steps):
     spline_points.append(Spline.spline(xData, yData, i * STEP, DIVISION_DENSITY))  # y(x)
+
+end = time.time()
+diff = end - start
+print("Czas trwania interpolacji funkcjami sklejanymi: {:10.4f} sekund".format(diff))
 
 x = np.arange(0, how_many_steps * STEP, STEP)
 y = np.array(spline_points).astype(np.float64)
